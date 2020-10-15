c2cfc5b0b8232947c7303f96a0dc8615330923e4
j_williamson9 <j_williamson9@outlook.com>
2020-10-15 22:29:47 +1000

    Added a timer for pop up and timer cancel

--- src/scripts.js
+++ src/scripts.js
@@ -2,8 +2,10 @@ $(document).ready(function () {
 
     setInterval(function () { $("#dialog").fadeIn(); }, 5000)
 
+
 });
 
 function Close() {
     $("#dialog").fadeOut();
+    clearInterval(function () { $("#dialog").fadeIn(); })
 }
