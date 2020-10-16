2f66edb0dc7200824190e1a30a1162cdd1b1639d
j_williamson9 <j_williamson9@outlook.com>
2020-10-16 11:57:57 +1000

    Revert "Merge branch 'JoshR' into master"
    This reverts commit 91d5ea39a8a7a8de0c568e224b60b4ad27e34b66, reversing
    changes made to c2cfc5b0b8232947c7303f96a0dc8615330923e4.

--- src/scripts.js
+++ src/scripts.js
@@ -1,15 +1,11 @@
 $(document).ready(function () {
-    
-    setInterval(function () { $("#mainBox").fadeIn(); }, 5000)
+
+    setInterval(function () { $("#dialog").fadeIn(); }, 5000)
 
 
 });
 
 function Close() {
-<<<<<<< HEAD
     $("#dialog").fadeOut();
     clearInterval(function () { $("#dialog").fadeIn(); })
-=======
-    $("#mainBox").fadeOut();
->>>>>>> JoshR
 }
