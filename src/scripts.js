4b6e6c3765529e51318db92ab2462fad6c9f7e17
j_williamson9 <j_williamson9@outlook.com>
2020-10-16 12:41:43 +1000

    Fixed JoshR branch not adding properly

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
-
-    setInterval(function () { $("#dialog").fadeIn(); }, 5000)
-
+    
+    setInterval(function () { $("#mainBox").fadeIn(); }, 5000)
 
 });
 
 function Close() {
-    $("#dialog").fadeOut();
-    clearInterval(function () { $("#dialog").fadeIn(); })
+    $("#mainBox").fadeOut();
 }
