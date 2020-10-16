1174461b3cb28739e48ccec9ba4ba9392d48ae9c
Josh <j.robertson@uq.net.au>
2020-10-16 15:04:57 +1000

    Close button logic

--- src/scripts.js
+++ src/scripts.js
@@ -1,15 +1,37 @@
-$(document).ready(function () {
-    
-    setInterval(function () { $(".awarePage").fadeIn(); }, 5000)
+var openPage = false;
 
+$(document).ready(function () {
+
+    console.log(openPage);
+    setInterval(checkPageOpen, 8000);
+    
+    function checkPageOpen() {
+        if (!openPage) {
+            $(".awarePage").fadeIn();
+            openPage = true;
+            console.log(openPage);
+        } else {
+            console.log(openPage);
+        }
+    }
 });
 
-function Close() {
-    $("#mainBox").fadeOut();
+function awareClose() {
+    $(".awarePage").fadeOut();
+    openPage = false;
+    console.log(openPage);
+}
+
+function createClose() {
+    $(".createPage").fadeOut();
+    openPage = false;
+    console.log(openPage);
 }
 
 function openCreatePage() {
     $(".awarePage").fadeOut();
     $(".awarePage").hide();
     $(".createPage").fadeIn();
+    openPage = true;
+    console.log(openPage);
 }
