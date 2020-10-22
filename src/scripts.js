46449d13af9936d24b7621264207cb217cec8f3d
j_williamson9 <j_williamson9@outlook.com>
2020-10-22 19:06:21 +1000

    Finished Picnic Activity Page

--- src/scripts.js
+++ src/scripts.js
@@ -3,7 +3,8 @@ var openPage = false;
 $(document).ready(function () {
 
     console.log(openPage);
-    setInterval(checkPageOpen, 8000);
+    setInterval(checkPageOpen, 8000)
+
     
     function checkPageOpen() {
         if (!openPage) {
@@ -28,6 +29,12 @@ function createClose() {
     console.log(openPage);
 }
 
+function picnicClose() {
+    $(".picnicPage").fadeOut();
+    openPage = false;
+    console.log(openPage);
+}
+
 function openCreatePage() {
     $(".awarePage").fadeOut();
     $(".awarePage").hide();
@@ -35,3 +42,27 @@ function openCreatePage() {
     openPage = true;
     console.log(openPage);
 }
+
+function openPicnicPage() {
+    $(".awarePage").fadeOut();
+    $(".awarePage").hide();
+    $(".picnicPage").fadeIn();
+    openPage = true;
+    console.log(openPage);
+}
+
+// Initialize and add the map
+function initMap() {
+    // The location of Brisbane Botantical Gardens
+    const botgardensbris = { lat: -27.475892, lng: 152.975028 };
+    // The map, centered at Brisbane Botantical Gardens
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 16,
+      center: botgardensbris,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: botgardensbris,
+      map: map,
+    });
+  }
