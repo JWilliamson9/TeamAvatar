f95fa1b283e30a2d2b04864dba44550651353508
Josh <j.robertson@uq.net.au>
2020-10-22 15:29:50 +1000

    friends invite page

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,7 @@
 var openPage = false;
+var inviteList = [];
+var invitedFriend1 = false;
+
 
 $(document).ready(function () {
 
@@ -28,10 +31,48 @@ function createClose() {
     console.log(openPage);
 }
 
+function inviteClose() {
+    $(".invitePage").fadeOut();
+    openPage = false;
+    console.log(openPage);
+}
+
 function openCreatePage() {
+    $(".createPage").fadeIn();
     $(".awarePage").fadeOut();
     $(".awarePage").hide();
-    $(".createPage").fadeIn();
     openPage = true;
     console.log(openPage);
+}
+
+function openInvitePage() {
+    $(".createPage").fadeOut();
+    $(".createPage").hide();
+    $(".invitePage").fadeIn();
+    openPage = true;
+    console.log(openPage);
+}
+
+function inviteFriend1() {
+    if (!invitedFriend1) {
+        invitedFriend1 = true;
+        document.getElementById("friend1").style.backgroundColor='#3f8fb4';
+        document.getElementById("friend1").innerHTML = "Invited";
+        inviteList.push(document.getElementById("friendPic1"));
+        console.log("friend1 " + invitedFriend1);
+        console.log(inviteList);
+    } else {
+        invitedFriend1 = false;
+        for( var i = 0; i < inviteList.length; i++){ 
+            console.log(i.id);
+            if (i.id = "friendPic1") { 
+                inviteList.splice(i, 1); i--; 
+            }
+        }
+        document.getElementById("friend1").style.backgroundColor='#5AC8FA';
+        document.getElementById("friend1").innerHTML = "Invite";
+        console.log("friend1 " + invitedFriend1);
+        console.log(inviteList);
+    }
+   
 }
